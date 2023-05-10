34c0af43b31074492e12b5ea29ed76bb00c0f91a
Angel Martinez <a_armando97@hotmail.com>
2023-05-10 08:57:01 -0600

    Endpoints y query parameters

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 //console.log('Cargado');
 
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&page=2';
 
 const getRandomCat = async function () {
-     let result = await fetch(URL);
+     let result = await fetch(API_URL);
      console.debug(result)
      let data = await result.json();
-     const img = document.querySelector('img');
-     img.src = data[0].url;
+     console.log(data);
+
+     const img1 = document.getElementById('img1');
+     const img2 = document.getElementById('img2');
+     const img3 = document.getElementById('img3');
+
+     img1.src  = data[0].url;
+     img2.src  = data[1].url;
+     img3.src  = data[2].url;
+     //const img = document.querySelector('img');
+     //img.src = data[0].url;
 } 
 
 getRandomCat();
